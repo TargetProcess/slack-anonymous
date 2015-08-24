ce7014a896d94f421930bbb0b0192967a59d9aa5
konstantin <krivlenia@targetprocess.com>
2015-08-24 12:04:58 +0300

    add support foreign characters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var requestify = require('requestify'); 
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -18,25 +18,25 @@ function getUsageHelp(commandName) {
     function createSample(target) {
         return commandName + ' *' + target + '* I know what you did last summer';
     }
-    
+
     var text = 'Expected usage: \n' +
         commandName + ' help -- Displays help message.\n' +
-        createSample('@user') + ' -- Sends to the specified user.\n' + 
+        createSample('@user') + ' -- Sends to the specified user.\n' +
         createSample('#channel') + ' -- Sends to the specified public channel.\n' +
         createSample('group') + ' -- Sends to the specified private group.\n' +
         createSample(':here') + ' -- Sends to the current group/channel/DM where you type this command.';
-        
+
     return text;
 }
 
 function getFullHelp(commandName) {
-    var text = 
-        'Allows to send anonymous messages to users, channels and groups.\n' + 
+    var text =
+        'Allows to send anonymous messages to users, channels and groups.\n' +
         'The most convenient and safe way is to open up a conversation with slackbot in Slack and type the commands there, so that nobody detects that you are typing and you don\'t accidentally reveal yourself by typing an invalid command.\n' +
         'Messages and authors are not stored, and the sources are available at <https://github.com/TargetProcess/slack-anonymous>.\n' +
-        '\n' + 
+        '\n' +
         getUsageHelp(commandName);
-        
+
     return text;
 }
 
@@ -44,51 +44,53 @@ function createResponsePayload(requestBody) {
     if (!requestBody) {
         return createError('Request is empty');
     }
-    
+
     var text = requestBody.text;
     var command = requestBody.command;
-    
+
     if (!text || text === 'help') {
         return createError(getFullHelp(command));
     }
-    
+
     var splitted = text.split(" ");
     if (splitted.length <= 1) {
         return createError(getUsageHelp(command));
     }
-    
+
     var target = splitted[0];
     var remainingText = splitted.slice(1).join(' ');
     remainingText = 'Someone said "' + remainingText + '"';
-    
+
     if (target === ':here') {
         return {
             channel: requestBody.channel_id,
             text: remainingText
         };
     }
-    
+
     return {
         channel: target,
         text: remainingText
     };
 }
 
-app.post('/', function(request, response) {
-    var body = request.body;
-    var command = body ? body.command : null;
-    command = command || '/anon';
-    
-    var payloadOption = createResponsePayload(request.body);
+app.post('/', function(req, response) {
+    var payloadOption = createResponsePayload(req.body);
     if (payloadOption.error) {
         response.end(payloadOption.error);
         return;
     }
-    
-    requestify.post(process.env.POSTBACK_URL, payloadOption).then(function() {
-        response.end('Delivered! :cop:');
-    }, function(error) {
-        response.end('Unable to post your anonymous message: ' + error.message + '\n' + getUsageHelp(command));
+    request({
+        url: process.env.POSTBACK_URL,
+        json: payloadOption,
+        method: 'POST'
+    }, function (error) {
+        if(error) {
+            response.end('Unable to post your anonymous message: ' + JSON.stringify(error));
+        } else {
+            response.end('Delivered! :cop:');
+        }
+
     });
 });
 
